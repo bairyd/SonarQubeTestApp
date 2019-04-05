eb2b5cee4c06672519504b1cbfe961d78e858c7a
Zubair Davids <zubair@haefelesoftware.com>
2019-04-05 11:06:14 +0200

    inital commit

---
+++ CalculatorApp/Calculator.Tests/CalculatorTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator.Tests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        CalculatorApp.Calculator calc = new CalculatorApp.Calculator();
+
+        [TestMethod]
+        public void TestAddition()
+        {
+            int expected = 11;
+            int a = 5;
+            int b = 6;
+            int result = calc.add(5,6);
+            Assert.AreEqual(expected,result);
+        }
+    }
+}
---
+++ CalculatorApp/CalculatorApp/Calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp
+{
+    public class Calculator
+    {
+        public int add(int a, int b)
+        {
+            return a + b;
+        }public int subtract(int a, int b)
+        {
+            return a - b;
+        }public int multiply(int a, int b)
+        {
+            return a * b;
+        }public int divide(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
