5ff2efb0629e9e6e7dd05b2c7bae2ee234f4af6a
Zubair Davids <zubair@haefelesoftware.com>
2019-04-05 09:02:43 +0200

    Initial commit

---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,24 @@
+﻿namespace CalculatorApp
+{
+    public class Calculator
+    {
+        public int add(int a, int b) {
+            return a + b;
+        }
+
+        int subtract(int a, int b)
+        {
+            return a + b;
+        }
+
+        int multiply(int a, int b)
+        {
+            return a + b;
+        }
+
+        int divide(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CalculatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+
+            Console.WriteLine("Adding 1 + 2: " + calc.add(1, 2));
+        }
+    }
+}
